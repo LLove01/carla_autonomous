15b020cc4898ce24ae8c6839fc1412a593a6d46c
LLove01 <luka.lovenjak44@gmail.com>
2022-05-24 22:57:40 +0200

    Added rgb camera, decent object detection

--- lane_detection.py
+++ lane_detection.py
@@ -1,7 +1,6 @@
 import glob
 import os
 import sys
-from tkinter import Frame
 from matplotlib import image
 from matplotlib.colors import ColorConverter
 import numpy as np
@@ -10,6 +9,7 @@ import queue
 import random
 
 from pid_controller import *
+from tf_detection import *
 
 try:
     sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
@@ -19,6 +19,41 @@ try:
 except IndexError:
     pass
 import carla
+
+
+def process_image(image):
+    # try:
+    #     image.save_to_disk('output/%.6d' % image.frame,
+    #                        carla.ColorConverter.CityScapesPalette)
+    #     segmentation_img = cv2.imread('output/%.6d' % image.frame)
+    #     cv2.imshow('', segmentation_img)
+    #     cv2.waitKey(1)
+    # except:
+    #     print('Error processing image.')
+    # rgb camera returns 4 chanels
+    image = np.array(image.raw_data)
+    img = image.reshape((600, 800, 4))
+    # cutting out alpha chanel
+    img = img[:, :, :3]
+
+    img = show_inference(detection_model, img)
+
+    cv2.imshow('rgb camera', img)
+    cv2.waitKey(100)
+
+    # stencil = np.zeros_like(img[:, :, 0])
+    # polygon = np.array([[50, 570], [380, 360], [560, 360], [780, 570]])
+    # cv2.fillConvexPoly(stencil, polygon, (255))
+    # mask_img = cv2.bitwise_and(img[:, :, 0], img[:, :, 0], mask=stencil)
+    # cv2.imshow('Mask image', mask_img)
+    # ret, thresh = cv2.threshold(mask_img, 220, 255, cv2.THRESH_BINARY)
+    # cv2.imshow('Tresh', thresh)
+    # lines = cv2.HoughLinesP(thresh, 1, np.pi/180, 30, maxLineGap=200)
+
+    # if lines is not None:
+    #     for line in lines:
+    #         x1, y1, x2, y2 = line[0]
+    #         cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
 
 
 def main():
@@ -43,27 +78,21 @@ def main():
         vehicle_controller = PID_Controller(vehicle, args_acceleration={
                                             'K_P': 1, 'K_D': 0.0, 'K_I': 0.0}, args_direction={'K_P': 1, 'K_D': 0.0, 'K_I': 0.0})
 
+        # semantic camera
+        camera_bp = bp_lib.find('sensor.camera.rgb')
+        camera_bp.set_attribute('image_size_x', '800')
+        camera_bp.set_attribute('image_size_y', '600')
+        camera_bp.set_attribute('fov', '90')
+        camera_location = carla.Transform(carla.Location(x=1.5, z=2.4))
+        camera = world.spawn_actor(
+            camera_bp, camera_location, attach_to=vehicle)
+        camera.listen(lambda image: process_image(image))
+
         while True:
             waypoints = world.get_map().get_waypoint(vehicle.get_location())
             waypoint = np.random.choice(waypoints.next(0.3))
-            control_signal = vehicle_controller.run_step(50, waypoint)
+            control_signal = vehicle_controller.run_step(5, waypoint)
             vehicle.apply_control(control_signal)
-
-            # # semantic camera
-            # camera_bp = bp_lib.find('sensor.camera.semantic_segmentation')
-            # camera_bp.set_attribute('image_size_x', '800')
-            # camera_bp.set_attribute('image_size_y', '600')
-            # camera_bp.set_attribute('fov', '90')
-            # camera_location = carla.Transform(carla.Location(x=1.5, y=2.4))
-            # camera = world.spawn_actor(camera_bp, camera_location)
-            # camera.listen(lambda image: image.save_to_disk(
-            #     f'output/{image.frame}', carla.ColorConverter.CityScapesPalette))
-            # # depth camera
-            # depth_cam_bp = bp_lib.find('sensor.camera.depth')
-            # depth_cam_location = carla.Transform(carla.Location(x=1.5, y=2.4))
-            # camera = world.spawn_actor(depth_cam_bp, depth_cam_location)
-            # camera.listen(lambda image: image.save_to_disk(
-            #     f'output/{image.frame}', carla.ColorConverter.LogarithmicDepth))
 
     finally:
         print('destroying actors')
